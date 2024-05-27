4acb5b28b0319ffae11784ca8a2c7f7e0f165135
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-28 03:55:44 +0400

    Add coc completion

---
+++ nvim/lua/mohannad/plugins/coc.lua
@@ -0,0 +1,18 @@
+return {
+	"neoclide/coc.nvim",
+	lazy = true,
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		vim.g.coc_global_extensions = {
+			"coc-json",
+			"coc-tsserver",
+			"coc-eslint",
+			"coc-prettier",
+		}
+
+		vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "gy", "<Plug>(coc-type-definition)", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "gi", "<Plug>(coc-implementation)", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "gr", "<Plug>(coc-references)", { noremap = true, silent = true })
+	end,
+}
