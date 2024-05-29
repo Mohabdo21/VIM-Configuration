09d2e07ce8e9bf29927c50c8fd79a1c26de1ef80
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-30 01:48:00 +0400

    Add keybinding to help jump back and forward with documentations

--- nvim/lua/mohannad/plugins/coc.lua
+++ nvim/lua/mohannad/plugins/coc.lua
@@ -17,6 +17,7 @@ return {
 		vim.api.nvim_set_keymap("n", "gy", "<Plug>(coc-type-definition)", { noremap = true, silent = true })
 		vim.api.nvim_set_keymap("n", "gi", "<Plug>(coc-implementation)", { noremap = true, silent = true })
 		vim.api.nvim_set_keymap("n", "gr", "<Plug>(coc-references)", { noremap = true, silent = true })
-		vim.api.nvim_set_keymap("n", "<leader>c", "<Esc>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "<Leader>o", "<C-o>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "<Leader>i", "<C-i>", { noremap = true, silent = true })
 	end,
 }
