0f6302175421dae3e651d39eb6669dd0370bdb48
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-02 05:48:37 +0400

    Enable 'file watching' feature in LSP configuration

--- nvim/lua/mohannad/plugins/lsp/lspconfig.lua
+++ nvim/lua/mohannad/plugins/lsp/lspconfig.lua
@@ -72,6 +72,12 @@ return {
 		-- used to enable autocompletion (assign to every lsp server config)
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
+		-- Ensure workspace is initialized
+		capabilities.workspace = capabilities.workspace or {}
+		capabilities.workspace.didChangeWatchedFiles = {
+			dynamicRegistration = true,
+		}
+
 		-- Change the Diagnostic symbols in the sign column (gutter)
 		-- (not in youtube nvim video)
 		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
