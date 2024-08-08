d9a28c59cee30592529802fbbcde460735d87186
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-08-08 15:53:00 +0400

    Fix session options

--- nvim/lua/mohannad/plugins/auto-session.lua
+++ nvim/lua/mohannad/plugins/auto-session.lua
@@ -8,6 +8,8 @@ return {
 			auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
 		})
 
+		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
 		local keymap = vim.keymap
 
 		keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
