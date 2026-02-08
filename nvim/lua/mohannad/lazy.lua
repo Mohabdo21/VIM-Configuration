f4b372f5bdaef023663692ce6e92c1b6164ffae4
Mohabdo21 <mohannadabdo21@hotmail.com>
2026-02-09 02:41:55 +0400

    chore: Bootstrap lazy.nvim plugin with improved cloning process

--- nvim/lua/mohannad/lazy.lua
+++ nvim/lua/mohannad/lazy.lua
@@ -1,17 +1,17 @@
--- Define the path to the lazy.nvim plugin
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
--- Check if the lazy.nvim plugin is already installed
-if not vim.loop.fs_stat(lazypath) then
-	-- If not, clone the plugin from GitHub
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- Use the latest stable release
-		lazypath,
-	})
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 
 -- Add the lazy.nvim plugin to the runtime path
